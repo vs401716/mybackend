926f3dee469b02cc898f0c062d3c6b9fc40c7b5f
vs401716 <vs401716@gmail.com>
2022-10-17 23:33:32 +0530

    this is postman

---
+++ src/routes/movies.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+/////////////////111////////
+// router.get('/movies',function(req,res){
+//     console.log("list of movies",req.params)
+//     const list_of_movies=["kgf","ironman","thor","ghajini","bahubali"]
+//     res.send(list_of_movies)
+//     })
+
+//////////////////22222///////////////
+// router.get('/movies/:index',function(req,res){
+//     console.log("list of movies",req.params)
+//     let list_of_movies=["kgf","ironman","thor","ghajini","bahubali"]
+//     let index = req.params.index
+//     res.send(list_of_movies[index])
+//     })
+
+
+
+    ///////////////////////////3333333///////////////
+    // router.get('/movies/:indexNumber', (req,res) => {
+    //     let index = req.params.indexNumber
+    //     movies=["kgf","ironman","thor","ghajini","bahubali"]
+    //     if(index>movies.length){
+    //         res.send('Please use a valid index')
+    //     }
+    //     else{
+    //         res.send(movies[index])  
+    //     }
+    // })
+// ///4//////////////////////44444444444///////
